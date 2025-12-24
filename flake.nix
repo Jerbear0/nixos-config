a0e98971b69d750cf8e931430bca7975b9264e71
Jerbear0 <albertjeremy3@gmail.com>
2025-12-24 02:56:29 -0500

    nixpkgs-xr properly added

--- flake.nix
+++ flake.nix
@@ -1,77 +1,64 @@
 {  
   description = "NixOS config with Home Manager + Hyprland";  
   
-  inputs = {  
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";  
-    nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";  
-    home-manager.url = "github:nix-community/home-manager/release-25.11";  
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";  
-  };  
+inputs = {  
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";  
+  nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";  
+  home-manager.url = "github:nix-community/home-manager/release-25.11";  
+  home-manager.inputs.nixpkgs.follows = "nixpkgs";  
+};  
   
-  outputs = { self, nixpkgs, nixpkgs-xr, home-manager, ... }:  
-    let  
-      system = "x86_64-linux";  
-      lib = nixpkgs.lib;  
-      pkgs = import nixpkgs {  
+outputs = { self, nixpkgs, nixpkgs-xr, home-manager, ... }:  
+  let  
+    system = "x86_64-linux";  
+    lib = nixpkgs.lib;  
+  in {  
+    nixosConfigurations = {  
+      nixos-desktop = lib.nixosSystem {  
         inherit system;  
-        config.allowUnfree = true;  
-      };  
-      pkgs-xr = import nixpkgs-xr {  
-        inherit system;  
-      };  
-    in {  
-      nixosConfigurations = {  
-        nixos-laptop = lib.nixosSystem {  
-          inherit system;  
   
-          specialArgs = {  
-            hostRole = "laptop";  
-            inherit pkgs-xr;  
-          };  
-  
-          modules = [  
-            ./configuration.nix  
-            ./hosts/laptop.nix  
-  
-            home-manager.nixosModules.home-manager  
-  
-            ({ pkgs, hostRole, pkgs-xr, ... }: {  
-              home-manager.useGlobalPkgs = true;  
-              home-manager.useUserPackages = true;  
-  
-              home-manager.users.jay = { pkgs, ... }:  
-                import ./home.nix {  
-                  inherit pkgs hostRole pkgs-xr;  
-                };  
-            })  
-          ];  
+        specialArgs = {  
+          hostRole = "desktop";  
         };  
   
-        nixos-desktop = lib.nixosSystem {  
-          inherit system;  
+        modules = [  
+          ./configuration.nix  
+          ./hosts/desktop.nix    
+
+          nixpkgs-xr.nixosModules.nixpkgs-xr  
+          home-manager.nixosModules.home-manager  
   
-          specialArgs = {  
-            hostRole = "desktop";  
-            inherit pkgs-xr;  
-          };  
+          ({ pkgs, hostRole, ... }: {  
+            home-manager.useGlobalPkgs = true;  
+            home-manager.useUserPackages = true;  
   
-          modules = [  
-            ./configuration.nix  
-            ./hosts/desktop.nix  
+            home-manager.users.jay = { pkgs, ... }:  
+              import ./home.nix {  
+                inherit pkgs hostRole;  
+              };  
+          })  
+        ];  
+      };  
   
-            home-manager.nixosModules.home-manager  
-  
-            ({ pkgs, hostRole, pkgs-xr, ... }: {  
-              home-manager.useGlobalPkgs = true;  
-              home-manager.useUserPackages = true;  
-  
-              home-manager.users.jay = { pkgs, ... }:  
-                import ./home.nix {  
-                  inherit pkgs hostRole pkgs-xr;  
-                };  
-            })  
-          ];  
-        };  
+      # (same idea if you also want XR on laptop)  
+      nixos-laptop = lib.nixosSystem {  
+        inherit system;  
+        specialArgs = { hostRole = "laptop"; };  
+        modules = [  
+          ./configuration.nix  
+          ./hosts/laptop.nix  
+
+          nixpkgs-xr.nixosModules.nixpkgs-xr  
+          home-manager.nixosModules.home-manager  
+
+          ({ pkgs, hostRole, ... }: {  
+            home-manager.useGlobalPkgs = true;  
+            home-manager.useUserPackages = true;  
+            home-manager.users.jay = { pkgs, ... }:  
+              import ./home.nix { inherit pkgs hostRole; };  
+          })  
+        ];  
       };  
     };  
-}   
+  }; 
+}    
