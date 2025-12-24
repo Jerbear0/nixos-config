0d1fb1611b3ce9339eb33f885570342ca5483857
Jerbear0 <albertjeremy3@gmail.com>
2025-12-23 19:52:53 -0500

    VRCFT Finally working

--- flake.nix
+++ flake.nix
@@ -5,19 +5,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";  
     home-manager.url = "github:nix-community/home-manager/release-25.11";  
     home-manager.inputs.nixpkgs.follows = "nixpkgs";  
-
-#    vrcftAvalonia.url = "github:dfgHiatus/VRCFaceTracking.Avalonia/v1.1.1.0";  
-#    vrcftAvalonia.inputs.nixpkgs.follows = "nixpkgs";
   };  
   
   outputs = { self, nixpkgs, home-manager, ... }:  
     let  
       system = "x86_64-linux";  
-      lib = nixpkgs.lib;   
-         pkgs = import nixpkgs {  
-           inherit system;  
-           config.allowUnfree = true;
-         };
+      lib = nixpkgs.lib;  
+      pkgs = import nixpkgs {  
+        inherit system;  
+        config.allowUnfree = true;  
+      };  
     in {  
       nixosConfigurations = {  
         nixos-laptop = lib.nixosSystem {  
@@ -28,7 +25,6 @@
           };  
   
           modules = [  
-          # { nixpkgs.overlays = overlays; } 
             ./configuration.nix  
             ./hosts/laptop.nix  
   
@@ -54,7 +50,6 @@
           };  
   
           modules = [  
-           # { nixpkgs.overlays = overlays; } 
             ./configuration.nix  
             ./hosts/desktop.nix  
   
@@ -74,4 +69,3 @@
       };  
     };  
 }  
- 
---
+++ modules/vrcft-avalonia.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:  
+  
+let  
+  cfg = config.programs.vrcft-avalonia;  
+    
+  vrcft-script = pkgs.writeShellScriptBin "vrcft-avalonia" ''  
+    # 1. Find ICU library path  
+    ICU_PATH=$(${pkgs.nix}/bin/nix-build --no-out-link '<nixpkgs>' -A icu)/lib  
+    export LD_LIBRARY_PATH="$ICU_PATH:$LD_LIBRARY_PATH"  
+  
+    # 2. Copy AppImage to /tmp so the sandbox can access it  
+    # This avoids the "bwrap: Can't chdir to /etc/nixos" error  
+    TEMP_APPIMAGE="/tmp/vrcft-avalonia-$(id -u).AppImage"  
+      
+    if [ ! -f "$TEMP_APPIMAGE" ]; then  
+      cp /etc/nixos/pkgs/VRCFaceTracking.Avalonia.1.1.1.0.AppImage "$TEMP_APPIMAGE"  
+      chmod +x "$TEMP_APPIMAGE"  
+    fi  
+      
+    # 3. Execute from /tmp  
+    exec ${pkgs.appimage-run}/bin/appimage-run "$TEMP_APPIMAGE" "$@"  
+  '';  
+in  
+{  
+  options.programs.vrcft-avalonia = {  
+    enable = lib.mkEnableOption "VRCFaceTracking.Avalonia via local AppImage";  
+  };  
+  
+  config = lib.mkIf cfg.enable {  
+    environment.systemPackages = [  
+      vrcft-script  
+    ];  
+  };  
+}  
