1fa79273cdf1bb536fd8a696e97690ed8e61eca9
Jerbear0 <albertjeremy3@gmail.com>
2026-03-31 19:53:15 -0400

    switched to lqx kernel, 6.18.8

--- configuration.nix
+++ configuration.nix
@@ -54,6 +54,8 @@
     nvidia.acceptLicense = true;  
   };   
 
+  boot.kernelPackages = pkgs.linuxPackages_lqx;
+
   nix.settings = {
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
